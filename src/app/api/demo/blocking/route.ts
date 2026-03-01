1a36712350065997c71447eeaa6269f077ffb3cc
ShivamDoifode <shivamarun888@gmail.com>
2026-03-01 14:11:39 +0530

    04-background jobs

---
+++ src/app/api/demo/blocking/route.ts
@@ -0,0 +1,13 @@
+// out path is localhost:3000/api/demo/blocking , this POST req on this route will prompt gemini ai
+
+import { google } from "@ai-sdk/google";
+import { generateText } from "ai";
+
+export async function POST() {
+  const response = await generateText({
+    model: google("gemini-2.5-flash"),
+    prompt: "Write a vegetarian lasagna recipe for 4 people.",
+  });
+
+  return Response.json({ response });
+}
