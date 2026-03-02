76f479f18b99189f65b7a9cfcdb9f35146f883fd
ShivamDoifode <shivamarun888@gmail.com>
2026-03-02 17:02:40 +0530

    06: Error Tracking

--- src/inngest/client.ts
+++ src/inngest/client.ts
@@ -1,4 +1,8 @@
+import { sentryMiddleware } from "@inngest/middleware-sentry";
 import { Inngest } from "inngest";
 
 // Create a client to send and receive events
-export const inngest = new Inngest({ id: "lucent" });
+export const inngest = new Inngest({
+  id: "lucent",
+  middleware: [sentryMiddleware()],
+});
